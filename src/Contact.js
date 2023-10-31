b3775e70bf80d9994d384f4bc96c634121c8351d
Angelo Alcera Cabaluna <cabsgelo561561@gmail.com>
2023-10-31 21:35:27 +0800

    update

--- src/Contact.js
+++ src/Contact.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import './App.css'; // Import the CSS file
 
 const Contact = () => {
   return (
-    <section className="contact">
-      <h2>Contact Me</h2>
-      <p>Email: example@example.com</p>
-      <p>LinkedIn: linkedin.com/in/example</p>
-    </section>
+    <div className="contact-container">
+      <h2 className="contact-heading">Contact Me</h2>
+      <p className="contact-info">Email: your.email@example.com</p>
+      <p className="contact-info">LinkedIn: <a href="here">Your LinkedIn Profile</a></p>
+      <button className="contact-button">Send Message</button>
+    </div>
   );
 };
 
