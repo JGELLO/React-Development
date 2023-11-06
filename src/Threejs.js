b2ee2e7abca98d8f17efd3deba32d79f04be80e8
Angelo Alcera Cabaluna <cabsgelo561561@gmail.com>
2023-11-07 00:52:37 +0800

    minor updates

--- src/Threejs.js
+++ src/Threejs.js
@@ -1,7 +1,16 @@
 import React from 'react';
 
 const Threejs = () => {
-  return <div>Threejs built</div>;
+  return (
+    <section className="threejs">
+      <h2>Threejs</h2>
+      <ul>
+
+        <li>Visual Studio Code</li>
+
+      </ul>
+  </section>
+  );
 };
 
 export default Threejs;
