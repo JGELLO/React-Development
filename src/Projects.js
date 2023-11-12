ef69f2be14c21c8b9845731a77fa7a03879730a9
Angelo Alcera Cabaluna <cabsgelo561561@gmail.com>
2023-11-12 16:23:28 +0800

    minor updates

--- src/Projects.js
+++ src/Projects.js
@@ -5,9 +5,9 @@ const Projects = () => {
     <section className="projects">
       <h2>Projects</h2>
       <ul>
-        <li>Project 1: WordPress site for Company</li>
-        <li>Project 2: Reactjs Portfolio - In development</li>
-        <li>Project 3: React Native App - In development</li>
+        <li>Project 1: WordPress site for Company -<a href="https://stnicolasinn.com/" target="_blank" rel="noopener noreferrer">St. Nicolas Inn  </a></li>
+        <li>Project 2: Reactjs Portfolio - In development -<a href="https://jgello.github.io/React-Pages/" target="_blank" rel="noopener noreferrer">GitHub Pages</a></li>
+        <li>Project 3: React Native App - In development -</li>
         {/* Add more projects as needed */}
         
       </ul>
