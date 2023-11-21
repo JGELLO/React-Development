878b0258a80106601d29e8a1bf9c4b5c828d0c33
Angelo Alcera Cabaluna <cabsgelo561561@gmail.com>
2023-11-21 11:41:37 +0800

    minor updates

--- src/LoadingScreen.js
+++ src/LoadingScreen.js
@@ -1,27 +1,11 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import Carousel from './Carousel'; // Replace this import with the actual path to your Carousel component
 import './LoadingScreen.css'; // Create a CSS file for styling
 
 const LoadingScreen = () => {
-  const [showHello, setShowHello] = useState(false);
-
-  useEffect(() => {
-    // Display the hello message after a delay
-    const timeout = setTimeout(() => {
-      setShowHello(true);
-    }, 9000); // Adjust the delay time as needed
-
-    return () => clearTimeout(timeout);
-  }, []);
-
   return (
     <div className="loading-screen">
       <Carousel />
-      {showHello && (
-        <div className="hello-message">
-          <h1>Hello!</h1>
-        </div>
-      )}
     </div>
   );
 };
