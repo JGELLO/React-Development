a14dbcbede4ffd79038f453d91020ec85e016c27
JGELLO <cabsgelo561561@gmail.com>
2024-02-02 14:59:43 +0800

    seo

--- src/SEOP.js
+++ src/SEOP.js
@@ -8,16 +8,27 @@ const SEO = () => {
       SEO (Search Engine Optimization) enhances a website's visibility on search engine results pages (SERPs) through keyword optimization, content improvement, and technical enhancements. It drives organic traffic, boosts user engagement, and increases conversions by aligning with search engine algorithms. SEO is an ongoing process, requiring constant monitoring and adaptation to maintain and improve rankings, ultimately contributing to a website's success in the digital sphere
       </p>
       <ul>
+        <br></br>
+      <h2><a href="https://stnicolasinn.com/" target="_blank" rel="noopener noreferrer">St. Nicolas Inn</a></h2>
+        <br></br>
         <li>
-          <strong>Project 1:</strong> WordPress Site Optimization for Company A - Implemented SEO strategies, improved site speed, and optimized content. Deployed and ongoing updates. <a href="https://example1.com/" target="_blank" rel="noopener noreferrer">Visit Site</a>
-        </li>
-        <li>
-          <strong>Project 2:</strong> React Portfolio Development for Company B - Developed a responsive portfolio website using React.js. Integrated SEO best practices and ongoing optimization efforts. <a href="https://example2.com/" target="_blank" rel="noopener noreferrer">Visit Site</a>
-        </li>
-        <li>
-          <strong>Project 3:</strong> SEO Audit and Strategy Development for Company C - Conducted an in-depth SEO audit, identified optimization opportunities, and developed a comprehensive SEO strategy for improved organic visibility. <a href="https://example3.com/" target="_blank" rel="noopener noreferrer">Visit Site</a>
+         WordPress Site Optimization for Company- Implemented SEO strategies, improved site speed, and optimized content.<a href="https://stnicolasinn.com/" target="_blank" rel="noopener noreferrer">Visit Site</a>
         </li>
         {/* Add more projects as needed */}
+        <br></br>
+        <h3>Strategies</h3>
+         <p>
+         SEO (Search Engine Optimization) enhances a website's visibility on search engine results pages (SERPs) through keyword optimization, content improvement, and technical enhancements. It drives organic traffic, boosts user engagement, and increases conversions by aligning with search engine algorithms. SEO is an ongoing process, requiring constant monitoring and adaptation to maintain and improve rankings, ultimately contributing to a website's success in the digital sphere
+          </p>
+          <br></br>
+        <h3>SEO Tools Used</h3>
+        
+            <li>
+              <p>Screaming Frog, Google Analytics, Ahref, SemRush, A variety of extension for crawling and checking usabilty.</p>
+            </li>
+          
+        
+        
       </ul>
     </section>
   );
