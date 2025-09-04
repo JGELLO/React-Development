61d95f6fffc448a120bc5e14cc3cee7dac8983ca
Angelo <angeloalceracabaluna@gmail.com>
2025-09-04 10:31:35 +0800

    update

--- src/App.js
+++ src/App.js
@@ -55,7 +55,6 @@ const App = () => {
             toggleMobileMenu={toggleMobileMenu}
           />
           <About />
-          <SEOP />
           <Skills />
           <Subskills />
           <Tools />
