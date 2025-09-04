e7b4b53206581a204c927ee48b40f16919af57c1
Angelo <angeloalceracabaluna@gmail.com>
2025-09-04 10:10:50 +0800

    update

--- src/SEOP.js
+++ src/SEOP.js
@@ -1,104 +1,108 @@
-import React from 'react';
+// import React from 'react';
 
-const SEO = () => {
-  return (
-    <section className="seo">
-      <h2>SEO Information</h2>
-      <li>
-        <p>
-        With a deep understanding of search engine algorithms and a knack for strategic optimization, I specialize in driving organic growth and maximizing online visibility through SEO. Armed with expertise in keyword research, on-page optimization, and technical SEO, I craft tailored strategies to propel businesses to the forefront of search engine results. Let's collaborate to unlock the power of SEO and elevate your online presence to new heights of success!
-        </p>
-      </li>
-      <ul>
-        <br></br>
-      <h2><a href="https://stnicolasinn.com/" target="_blank" rel="noopener noreferrer">St. Nicolas Inn</a></h2>
-        <br></br>
-        <li>
-         WordPress Site Optimization for Company- Implemented SEO strategies, improved site speed, and optimized content.<a href="https://stnicolasinn.com/" target="_blank" rel="noopener noreferrer">Visit Site</a>
-        </li>
-        {/* Add more projects as needed */}
-        <br></br>
-        <h2>Strategies</h2>
 
-<h3>Objective:</h3>
-<li>
-  <p>To enhance online visibility and increase organic traffic for St. Nicolas Inn, a luxury hotel located in a prime tourist destination.</p>
-</li>
+// May update
 
-<h3>Strategies Implemented:</h3>
 
-<li>
-<p>
-        1. Keyword Research and Optimization:
-           - Conducted keyword research to identify relevant search terms.
-           - Optimized website content and meta tags strategically.
+// const SEO = () => {
+//   return (
+//     <section className="seo">
+//       <h2>SEO Information</h2>
+//       <li>
+//         <p>
+//         With a deep understanding of search engine algorithms and a knack for strategic optimization, I specialize in driving organic growth and maximizing online visibility through SEO. Armed with expertise in keyword research, on-page optimization, and technical SEO, I craft tailored strategies to propel businesses to the forefront of search engine results. Let's collaborate to unlock the power of SEO and elevate your online presence to new heights of success!
+//         </p>
+//       </li>
+//       <ul>
+//         <br></br>
+//       <h2><a href="https://stnicolasinn.com/" target="_blank" rel="noopener noreferrer">St. Nicolas Inn</a></h2>
+//         <br></br>
+//         <li>
+//          WordPress Site Optimization for Company- Implemented SEO strategies, improved site speed, and optimized content.<a href="https://stnicolasinn.com/" target="_blank" rel="noopener noreferrer">Visit Site</a>
+//         </li>
+//         {/* Add more projects as needed */}
+//         <br></br>
+//         <h2>Strategies</h2>
 
-        <br></br>
+// <h3>Objective:</h3>
+// <li>
+//   <p>To enhance online visibility and increase organic traffic for St. Nicolas Inn, a luxury hotel located in a prime tourist destination.</p>
+// </li>
 
-        2. On-Page SEO Optimization:
-           - Implemented on-page SEO best practices.
-           - Optimized URLs, meta descriptions, and image alt text.
+// <h3>Strategies Implemented:</h3>
 
-        <br></br>
+// <li>
+// <p>
+//         1. Keyword Research and Optimization:
+//            - Conducted keyword research to identify relevant search terms.
+//            - Optimized website content and meta tags strategically.
 
-        3. Content Creation and Blogging:
-           - Developed a content strategy focused on engaging blog posts.
-           - Produced high-quality content regularly.
+//         <br></br>
 
-        <br></br>
+//         2. On-Page SEO Optimization:
+//            - Implemented on-page SEO best practices.
+//            - Optimized URLs, meta descriptions, and image alt text.
 
-        4. Local SEO and Google My Business:
-           - Optimized Google My Business profile with accurate information.
-           - Implemented local SEO strategies and obtained positive reviews.
+//         <br></br>
 
-        <br></br>
+//         3. Content Creation and Blogging:
+//            - Developed a content strategy focused on engaging blog posts.
+//            - Produced high-quality content regularly.
 
-        5. Mobile Optimization:
-           - Ensured the website is fully optimized for mobile devices.
-           - Implemented responsive design and optimized page speed.
+//         <br></br>
 
-        <br></br>
+//         4. Local SEO and Google My Business:
+//            - Optimized Google My Business profile with accurate information.
+//            - Implemented local SEO strategies and obtained positive reviews.
 
-        6. Technical SEO Audits and Improvements:
-           - Conducted technical SEO audits to address website performance issues.
-           - Implemented technical improvements and resolved crawl errors.
-      </p>
-</li>
+//         <br></br>
 
-<h3>Results Achieved:</h3>
+//         5. Mobile Optimization:
+//            - Ensured the website is fully optimized for mobile devices.
+//            - Implemented responsive design and optimized page speed.
 
-<li>
-  <p>
-    1. Significant increase in organic search traffic to the St. Nicolas Inn website.
-    <br></br>
-    2. Improved search engine rankings for target keywords related to luxury accommodations and travel in the hotel's locality.
-    <br></br>
-    3. Enhanced online visibility and brand recognition for St. Nicolas Inn in the competitive hospitality industry.
-    <br></br>
-    4. Positive feedback from guests regarding the hotel's online presence and user experience.
-    <br></br>
-    5. Continued growth in website traffic and bookings, contributing to the hotel's overall business success.
-    <br></br>
-  </p>
-</li>
+//         <br></br>
 
-<h3>Conclusion:</h3>
-<li>
-  <p>The SEO strategies implemented for St. Nicolas Inn have successfully improved online visibility, increased organic traffic, and enhanced the hotel's digital presence. By focusing on keyword optimization, on-page SEO, content creation, local SEO, mobile optimization, backlink building, and technical improvements, St. Nicolas Inn has achieved significant growth in search engine rankings and website performance, ultimately driving more bookings and revenue for the hotel.</p>
-</li>
+//         6. Technical SEO Audits and Improvements:
+//            - Conducted technical SEO audits to address website performance issues.
+//            - Implemented technical improvements and resolved crawl errors.
+//       </p>
+// </li>
 
-          <br></br>
-        <h3>SEO Tools Used</h3>
+// <h3>Results Achieved:</h3>
+
+// <li>
+//   <p>
+//     1. Significant increase in organic search traffic to the St. Nicolas Inn website.
+//     <br></br>
+//     2. Improved search engine rankings for target keywords related to luxury accommodations and travel in the hotel's locality.
+//     <br></br>
+//     3. Enhanced online visibility and brand recognition for St. Nicolas Inn in the competitive hospitality industry.
+//     <br></br>
+//     4. Positive feedback from guests regarding the hotel's online presence and user experience.
+//     <br></br>
+//     5. Continued growth in website traffic and bookings, contributing to the hotel's overall business success.
+//     <br></br>
+//   </p>
+// </li>
+
+// <h3>Conclusion:</h3>
+// <li>
+//   <p>The SEO strategies implemented for St. Nicolas Inn have successfully improved online visibility, increased organic traffic, and enhanced the hotel's digital presence. By focusing on keyword optimization, on-page SEO, content creation, local SEO, mobile optimization, backlink building, and technical improvements, St. Nicolas Inn has achieved significant growth in search engine rankings and website performance, ultimately driving more bookings and revenue for the hotel.</p>
+// </li>
+
+//           <br></br>
+//         <h3>SEO Tools Used</h3>
         
-            <li>
-              <p>Screaming Frog, Google Analytics, Ahref, SemRush, A variety of extension for crawling and checking usabilty.</p>
-            </li>
+//             <li>
+//               <p>Screaming Frog, Google Analytics, Ahref, SemRush, A variety of extension for crawling and checking usabilty.</p>
+//             </li>
           
         
         
-      </ul>
-    </section>
-  );
-};
+//       </ul>
+//     </section>
+//   );
+// };
 
-export default SEO;
+// export default SEO;
